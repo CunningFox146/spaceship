f1d956798d7a5a9ec4654d44d954fc8b3398d349
CunningFox146 <dezhemesov.work@gmail.com>
2021-08-03 16:05:08 +0300

    Added animation curve for rotation speed

--- Assets/Scripts/Components/PlayerController.cs
+++ Assets/Scripts/Components/PlayerController.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Scripts.Components
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed = 1f;
-        [SerializeField] private float _rotationSpeed = 1f;
+        [SerializeField] private AnimationCurve _rotationCurve;
+        [SerializeField] private float _moveSpeed = 90f;
+        [SerializeField] private float _rotationSpeed = 180f;
 
         private Rigidbody _rb;
         private Quaternion _rotation;
+        private float _rotationTime;
 
         void Awake()
         {
@@ -19,9 +22,15 @@ namespace Scripts.Components
 
         void Update()
         {
-            float rotation = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
-            float speed = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            float rotation = inputH * _moveSpeed * Time.deltaTime;
+            float speed = inputV * _moveSpeed * Time.deltaTime;
             float angle = rotation * _rotationSpeed * Time.deltaTime;
+
+
+            _rotationTime = inputH > 0f ? _rotationTime + Time.deltaTime : 0f;
 
             Rotate(angle);
             Move(speed);
@@ -29,7 +38,9 @@ namespace Scripts.Components
 
         private void Rotate(float deltaAngle)
         {
-            transform.rotation *= Quaternion.AngleAxis(deltaAngle, Vector3.up);
+            float speedFactor = _rotationCurve.Evaluate(_rotationTime);
+            Debug.Log(speedFactor);
+            transform.rotation *= Quaternion.AngleAxis(deltaAngle * speedFactor, Vector3.up);
         }
 
         private void Move(float speed)
