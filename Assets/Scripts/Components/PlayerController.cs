127e65d10b26e560ddd520bc7f04110cd882ea41
CunningFox146 <dezhemesov.work@gmail.com>
2021-08-03 15:18:49 +0300

    Reworked player controller

--- Assets/Scripts/Components/PlayerController.cs
+++ Assets/Scripts/Components/PlayerController.cs
@@ -6,21 +6,35 @@ namespace Scripts.Components
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed = 10f;
+        [SerializeField] private float _moveSpeed = 1f;
+        [SerializeField] private float _rotationSpeed = 1f;
 
         private Rigidbody _rb;
+        private Quaternion _rotation;
 
-        void Start()
+        void Awake()
         {
             _rb = GetComponent<Rigidbody>();
         }
 
         void Update()
         {
-            float speedX = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
-            float speedY = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
-            
-            _rb.velocity = new Vector3(speedX, speedY);
+            float rotation = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+            float speed = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+            float angle = rotation * _rotationSpeed * Time.deltaTime;
+
+            Rotate(angle);
+            Move(speed);
+        }
+
+        private void Rotate(float deltaAngle)
+        {
+            transform.rotation *= Quaternion.AngleAxis(deltaAngle, Vector3.up);
+        }
+
+        private void Move(float speed)
+        {
+            _rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
         }
     }
 }
